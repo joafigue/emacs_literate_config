34bbeafb0641dc60fc937d33029c4f122ea6896f
joafigue <joafigue@gmail.com>
2018-11-05 15:54:59 -0300

    New version with additional features from org-ref
    reverted back to use-package.
    Added org-ref configuration (used for master thesis)
    Included additional packages

--- init.el
+++ init.el
@@ -2,21 +2,24 @@
 ;;; init.el --- Load all org-related files
 ;; This is the first thing to get loaded.
 ;;
-;; Ensure req-package is available from melpa-stable
+;; Ensure use-package is available from melpa-stable
 (package-initialize)
-(unless (require 'req-package nil 'noerror)
-  (require 'package)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
   (add-to-list 'package-archives
                '("melpa-stable" . "http://stable.melpa.org/packages/") t)
   (package-refresh-contents)
   (package-initialize)
-  (package-install 'req-package)
-  (require 'req-package))
+  (package-install 'use-package))
+
 
 ;; Setting directories to load files
 (setq emacs-files-dir (file-name-directory (or load-file-name (buffer-file-name))))
 
 ;; Load up Org Mode and Babel
-(req-package org)
+(use-package org)
 ;; Load org-files
-   (org-babel-load-file (expand-file-name "Main.org" emacs-files-dir))
+(org-babel-load-file (expand-file-name "Main.org" emacs-files-dir))
+(org-babel-load-file (expand-file-name "jf_org_ref_reftex_setup.org" emacs-files-dir))
